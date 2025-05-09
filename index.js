5ff10e8733c22d2899b483c9a0ccb968919f0adb
Mykhailo <ignatyuk71@gmail.com>
2025-05-09 08:39:09 +0200

    Add .env to .gitignore

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Додаємо middleware для автоматичного парсингу JSON-запитів
 app.use(express.json());
