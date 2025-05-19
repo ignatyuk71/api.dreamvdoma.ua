9eadde886c603f2067a21963e1fc886c6b498ac7
Mykhailo <ignatyuk71@gmail.com>
2025-05-19 17:07:49 +0200

    Add .env to .gitignore

--- index.js
+++ index.js
@@ -68,8 +68,7 @@ app.post('/api/pageView', async (req, res) => {
           event_source_url: event.event_source_url || req.headers.referer || "",
           user_data: userData
         }
-      ],
-      test_event_code: "TEST13640" // ✅ Додано код тестування
+      ]
     };
   
     // Логування payload
